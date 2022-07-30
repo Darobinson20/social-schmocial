ecb26ef14aa7e2ffc5439d3f49fb2c55ae045a4b
Darobinson20 <Darobinson20@gmail.com>
2022-07-30 03:47:40 -0400

    user model started

--- models/user.js
+++ models/user.js
@@ -1,4 +1,51 @@
-const Pizza = require('./Pizza');
-const Comment = require('./Comment');
+const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
 
-module.exports = { Pizza, Comment };
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought'
+      }
+    ],
+    friends: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: 'User'
+        }
+      ]
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+    // prevents virtuals from creating duplicate of _id as `id`
+    id: false
+  }
+);
+
+// get total count of comments and replies on retrieval
+UserSchema.virtual('friendCount').get(function() {
+  return this.friends.length(
+    (total, comment) => total + comment.replies.length + 1,
+    0
+  );
+});
+
+const User = model('User', UserSchema);
+
+module.exports = User;
