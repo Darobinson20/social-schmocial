22c2bf225240e8a2927687aa97319e21fae8ad79
Darobinson20 <Darobinson20@gmail.com>
2022-08-02 02:01:55 -0400

    push

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,34 +1,20 @@
-const router = require('express').Router();
+const router = require("express").Router();
+
 const {
   getAllUsers,
   getUserById,
   updateUser,
   createUser,
-  deleteUser
-} = require('../../controllers/user-controller');
+  deleteUser,
+} = require("../../controllers/user-controller");
 
+//router.route("/").get().post();
 
-router
-  .route('/')
-  .get()
-  .post();
+router.route("/:id")
+.get(getUserById)
+.put(updateUser)
+.delete(deleteUser);
 
-
-router
-  .route('/:id')
-  .get(getUserById)
-  .put(updateUser)
-  .delete(deleteUser);
-
-  router
-  .route('/:id')
-  .put(updateUser)
-  .delete(deleteUser);
-
-  router
-  .route('/')
-  .get(getAllUsers)
-  .post(createUser);
-
+router.route("/").get(getAllUsers).post(createUser);
 
 module.exports = router;
